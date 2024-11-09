aac7abe7477f0cf7fd0bd8f3ffd4cbe6b8b4a43e
Su-Hyun Park <lovehyun@hotmail.com>
2024-11-09 20:32:30 +0900

    [BUG] 마지막 한명 결과만 사진에 출력되는 버그 수정 - 여러명의 등장인물이 있을 경우 모두다 동일 사진에 누적 출력

--- 9.cloud/9.2.detect_faces.py
+++ 9.cloud/9.2.detect_faces.py
@@ -3,11 +3,12 @@ import sys
 import pprint
 from s3_wrapper import upload_image, delete_image
 from pil_wrapper import mark_face_image
+from PIL import Image
 
 BUCKET = 'shpark-rekognition' # put your own bucket here
 KEY = ''
 
-FEATURES_IGNORED = ('Landmarks', 'Pose', 'Quality', 'BoundingBox', 'Confidence', 'Beard', 'Mustache', 'EyesOpen', 'MouthOpen')
+FEATURES_TO_PRINT = ('Emotions', 'Gender', 'AgeRange')
 
 # https://docs.aws.amazon.com/ko_kr/rekognition/latest/dg/faces-detect-images.html
 def detect_faces(bucket, key, attributes=['ALL'], region='ap-northeast-2'):
@@ -31,21 +32,26 @@ if __name__ == '__main__':
 
     upload_image(BUCKET, KEY)
 
+    # 여러 얼굴을 표시하기 위해 원본 이미지를 한 번만 열기
+    image_with_all_faces = Image.open(KEY)
+
+    # 모든 얼굴에 대해 경계 상자와 텍스트 표시
     for face in detect_faces(BUCKET, KEY):
+        # print('---')
         # pprint.pprint(face)
-
+        
         # facial features
-        for feature, data in face.items():
-            print(feature)
-            if feature not in FEATURES_IGNORED:
-                print('\n' + feature + ':')
-                pprint.pprint(data)
+        # for feature, data in face.items():
+        #     # print(feature)
+        #     if feature in FEATURES_TO_PRINT:
+        #         print('\n' + feature + ':')
+        #         pprint.pprint(data)
 
-        # save image
-        text = []
-        for emo in face['Emotions']:
-            text.append("{Type:10}: {Confidence:5.2f}%\n".format(**emo))
+        text = ["{Type}: {Confidence:.2f}%".format(**emo) for emo in face['Emotions']]
+        mark_face_image(KEY, face['BoundingBox'], text, filename=None, image=image_with_all_faces)
 
-        mark_face_image(KEY, face['BoundingBox'], text, fontsize=12, filename='output.png')
+    # 모든 얼굴이 표시된 이미지 저장
+    output_filename = 'output_with_all_faces.png'
+    image_with_all_faces.save(output_filename)
 
     delete_image(BUCKET, KEY)
